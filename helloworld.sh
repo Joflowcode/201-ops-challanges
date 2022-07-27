8d71f9d982d1690e41ed31fc4898b34342cbce56
Jo Flow <joflowcode@gmail.com>
2022-07-27 11:24:12 +0000

    ops class 2 challenge

---
+++ helloworld.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Script: Ops 201 Class 02 Ops Challenge Solution
+# Author: Jo Flow
+# Date of latest revision:
+# Purpose: Print a sting to the terminal
+
+# Main
+
+echo "I am working perfectly"
