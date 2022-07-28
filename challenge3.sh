df597db9fd7067fa21bb5dbc8d31b56bd0f7976b
Jo Flow <joflowcode@gmail.com>
2022-07-28 10:07:08 +0000

    second experimental attempt challenge 3

--- challenge3.sh
+++ challenge3.sh
@@ -2,19 +2,21 @@
 
 # Script: Ops 201 Class 03 Ops Challenge Solution
 # Author: Jo Flow
-# Date of latest revision: 7/27/22
+# Date of latest revision: 7/27/22 
 # Purpose: Create a function
 
 # Main
 
-# Basic construct
-#function function_name {
-#   command
-#}
+#Attempt 2, keep getting error it is a directory. try to convert to a file?
+
+
 
 # declare variable
 
-loginhistory=(`cat ls /last`)
+userhistory=$(`cat ls /last`)
+
+touch user_login_history.txt
+#loginhistory=(`cat ls /last`)
 
 # declare function
 
@@ -22,8 +24,14 @@ function print_login_history {
   echo "$loginhistory"
 }
 
-#invoke function
-#print_login_history "cat ls /var/log "
+function create_file (
+  echo $userhistory
+)
+# create function that passes that to a text file somehow
 print_login_history
+
+
+
+
 
  
